7ec4a152f2bc2e6168edd0912baf55cc8f798513
agusanwar <agusanwar@gmail.com>
2022-11-21 11:15:42 +0700

    pul to refesh

--- lib/screens/full_to_refresh/full_to_refresh.dart
+++ lib/screens/full_to_refresh/full_to_refresh.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class FullToRefreshScreens extends StatefulWidget {
   const FullToRefreshScreens({super.key});
@@ -7,9 +8,89 @@ class FullToRefreshScreens extends StatefulWidget {
   State<FullToRefreshScreens> createState() => _FullToRefreshScreensState();
 }
 
+Future<void> _handleRefresh() async {
+  return await Future.delayed(Duration(seconds: 2));
+}
+
 class _FullToRefreshScreensState extends State<FullToRefreshScreens> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar: AppBar(
+        title: Text('Pull to Refresh'),
+        centerTitle: true,
+      ),
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        color: Colors.blue[700],
+        backgroundColor: Colors.blue[200],
+        height: 270,
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 200,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
